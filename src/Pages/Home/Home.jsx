9cf4416a5baad7270cafc38b586e835a2cee3670
Radwan Hassan <radhassan555@gmail.com>
2026-02-19 02:02:01 +0300

    product items

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,81 +13,91 @@ const Home = () => {
 
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event ,  newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
 
- 
+
   return (
 
     <>
-    <HomeSlider/>
-    <HomeCatSlider/>
+      <HomeSlider />
+      <HomeCatSlider />
 
 
 
-    <section className="bg-white py-8 px-10">
-      <div className="conainer-fuild">
-        <div className="flex items-center justify-between">
-          <div className="leftsec">
-            <h3 className="text-[22px] font-[600] ">Popular Products</h3>
-            <p className="text-[14px] font-[400] ">Do not miss the current offers until the end of March.</p>
+      <section className="bg-white pt-8 px-10">
+        <div className="conainer-fuild">
+          <div className="flex items-center justify-between">
+            <div className="leftsec">
+              <h3 className="text-[22px] font-[600] ">Popular Products</h3>
+              <p className="text-[14px] font-[400] ">Do not miss the current offers until the end of March.</p>
+            </div>
+            <div className="leftsec w-[66%]">
+
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons
+                aria-label="visible arrows tabs example"
+                sx={{
+                  [`& .${tabsClasses.scrollButtons}`]: {
+                    '&.Mui-disabled': { opacity: 0.3 },
+                  },
+                }}
+              >
+                <Tab label="Fashion" />
+                <Tab label="Electronics" />
+                <Tab label="Bags" />
+                <Tab label="FootWear" />
+                <Tab label="Groceries" />
+                <Tab label="Beauty" />
+                <Tab label="Wallness" />
+                <Tab label="jewellery" />
+              </Tabs>
+            </div>
           </div>
-          <div className="leftsec w-[66%]">
-           
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons
-        aria-label="visible arrows tabs example"
-        sx={{
-          [`& .${tabsClasses.scrollButtons}`]: {
-            '&.Mui-disabled': { opacity: 0.3 },
-          },
-        }}
-      >
-        <Tab label="Fashion" />
-        <Tab label="Electronics" />
-        <Tab label="Bags" />
-        <Tab label="FootWear" />
-        <Tab label="Groceries" />
-        <Tab label="Beauty" />
-        <Tab label="Wallness" />
-        <Tab label="jewellery" />
-      </Tabs>
-          </div>
+
+          <ProductsSlider items={6} />
         </div>
+      </section>
 
-        <ProductsSlider items={4}/>
-      </div>
-    </section>
+      <section className="bg-white py-5 pt-4">
+        <div className="container ">
+          <div className="FreeShiping w-[80%] p-4 border-2 border-[#ff5252] flex items-center justify-between rounded-md  m-auto ">
+            <div className="col1 flex items-center gap-4 " >
 
-    <section className="bg-white py-5">
-      <div className="container ">
-        <div className="FreeShiping w-[80%] p-4 border-2 border-[#ff5252] flex items-center justify-between rounded-md  m-auto ">
-          <div className="col1 flex items-center gap-4 " >
+              <LiaShippingFastSolid className="text-[50px]" />
+              <span className="text-[30px] font-[700] uppercase">Free Shipping</span>
 
-            <LiaShippingFastSolid className="text-[50px]"/>
-            <span className="text-[30px] font-[700] uppercase">Free Shipping</span>
-
+            </div>
+            <div className="col2">
+              <p>Free Delevry Now On Your First Order And Over $200</p>
+            </div>
+            <p className="font-bold text-[25px]">- Only $200*</p>
           </div>
-          <div className="col2">
-            <p>Free Delevry Now On Your First Order And Over $200</p>
-          </div>
-          <p className="font-bold text-[25px]">- Only $200*</p>
+          <AdsPanerSlider items={4} />
         </div>
-        <AdsPanerSlider items={4}/>
-      </div>
-    </section>
+      </section>
 
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+
+      <section className="pb-5 bg-white ">
+        <div className="container">
+          <h2 className="text-[20px] font-[600]">Leatst Products</h2>
+          <ProductsSlider items={6} />
+
+
+        </div>
+      </section>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }
--- src/components/ProductSlider/index.jsx
+++ src/components/ProductSlider/index.jsx
@@ -1,9 +1,74 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css/navigation';
+
+import "swiper/css";
+import "swiper/css/pagination";
+import { Navigation } from 'swiper/modules';
+import ProductItem from "../ProductItem";
+
 const ProductsSlider = (props) => {
-  return ( 
-    <div className="ProductsSlider">broduct
+  return (
+    <div className="ProductsSlider py-3">
+
+      <Swiper
+        slidesPerView={props.items}
+        spaceBetween={10}
+
+        navigation={true}
+        modules={[Navigation]}
+
+        className="mySwiper"
+      >
+        {/* {ProductCatSlider.map((productslide) => ( */}
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+        <SwiperSlide >
+          <ProductItem/>
+
+        </SwiperSlide>
+      </Swiper>
+
 
     </div>
-   );
+  );
 }
 
 export default ProductsSlider;
