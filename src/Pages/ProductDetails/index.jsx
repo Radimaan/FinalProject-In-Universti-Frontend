fb071ecfbc5879ab33e3f893c455280ca1dd4e18
Radwan Hassan <radhassan555@gmail.com>
2026-02-24 19:22:00 +0300

    detailse

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,33 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import { Link } from 'react-router-dom';
 import ProductZoom from '../../components/ProductZoom';
+import Rating from '@mui/material/Rating';
+import { Button } from '@mui/material';
+
 
 
 const ProductDelails = () => {
+
+  const [activeSize, setactivesize] = useState(false);
+
+  const handleClick = (size) => {
+    setActiveSize(size); // set the clicked size as active
+  };
+
   return (
-    <section className="py-5 pb-0">
-      <div className="container pl-2  ">
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link underline="hover" color="inherit" href="/" className="link">
-            Home
-          </Link>
-          <Link
-            underline="hover"
-            color="inherit"
-            href="/" className="link"
-          >
-            Fation
-          </Link>
-        </Breadcrumbs>
+    <>
+      <div className="py-5">
+        <div className="container pl-2  ">
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/" className="link">
+              Home
+            </Link>
+            <Link
+              underline="hover"
+              color="inherit"
+              href="/" className="link"
+            >
+              Fation
+            </Link>
+          </Breadcrumbs>
+        </div>
+
+
       </div>
 
-      <div className=" container flex flex-wrap gap-4 overflow-hidden">
-        <div className="productZoomContainer !w-[30%] !h-[30%] overflow-hidden">
-          <ProductZoom />
+      <section className='bg-white py-5'>
+        <div className=" container flex  gap-4">
+          <div className="productZoomContainer !w-[40%] ">
+            <ProductZoom />
+          </div>
+          <div className="productcontent !w-[60%] pr-20 ">
+            <h1 className='text-[25px] font-[700] !capitalize mb-3'>Chikankari Woven Kurta</h1>
+            <div className="flex items-center">
+              <span className='text-gray-400 text-[13px]'>Brands : <span className='font-[500] text-black capitalize opacity-75'>House of Chikankari
+              </span></span>
+              <Rating name="size-small" defaultValue={5} size="small" readOnly />
+
+              <span className='text-gray-400 text-[13px] cursor-pointer'>{" "} (7) Review</span>
+            </div>
+
+            <div className="flex items-center gap-4 my-3">
+              <span className="OldPrice line-through text-gray-500 text-[20px] font-[600]">$23.00</span>
+              <span className="NewPrice text-[#ff5252] text-[20px] font-[700]">$20.99</span>
+            </div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <div className="flex items-center my-3 gap-3">
+              <span>Size : </span>
+
+              <Button
+                className={`!w-8 h-8 !min-w-8 !text-lg !border !border-gray-300 ${activeSize === "S" ? "!bg-[#ff5252] !text-white" : "!text-black hover:!bg-[#f0631d10]"
+                  }`}
+                onClick={() => handleClick("S")}
+              >
+                S
+              </Button>
+
+              <Button
+                className={`!w-8 h-8 !min-w-8 !text-lg !border !border-gray-300 ${activeSize === "M" ? "!bg-[#ff5252] !text-white" : "!text-black hover:!bg-[#f0631d10]"
+                  }`}
+                onClick={() => handleClick("M")}
+              >
+                M
+              </Button>
+
+              <Button
+                className={`!w-8 h-8 !min-w-8 !text-lg !border !border-gray-300 ${activeSize === "L" ? "!bg-[#ff5252] !text-white" : "!text-black hover:!bg-[#f0631d10]"
+                  }`}
+                onClick={() => handleClick("L")}
+              >
+                L
+              </Button>
+            </div>
+            
+            <div className="flex items-center">
+
+            </div>
+
+          </div>
+
         </div>
-      </div>
-    </section>)
+      </section>
+    </>
+  )
 }
 
 export default ProductDelails
--- src/components/ProductZoom/index.jsx
+++ src/components/ProductZoom/index.jsx
@@ -1,18 +1,111 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import InnerImageZoom from "react-inner-image-zoom";
 import "react-inner-image-zoom/lib/styles.min.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
 const ProductZoom = () => {
-  const Zoom = InnerImageZoom.default || InnerImageZoom; // <- Tani ayaa saxaysa React 19 issue
+
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const zoomSliderBig = useRef(null);
+  const zoomSliderSml = useRef(null);
+
+  const goto = (index) => {
+    setSlideIndex(index);
+
+    if (zoomSliderSml.current?.swiper) {
+      zoomSliderSml.current.swiper.slideTo(index);
+    }
+
+    if (zoomSliderBig.current?.swiper) {
+      zoomSliderBig.current.swiper.slideTo(index);
+    }
+  };
+
+  const ProductCatSlider = [
+    {
+      id: 1,
+      image: "https://api.spicezgold.com/download/file_1734529362999_gosriki-women-s-pink-ethnic-motifs-printed-kurta-with-trouser-dupatta-product-images-rvpkyh5qdr-0-202310141511.webp"
+    },
+    {
+      id: 2,
+      image: "https://api.spicezgold.com/download/file_1734528862997_deel-band-women-rayon-embroidered-kurta-pant-dupatta-set-product-images-rvotwal6de-2-202404071601.jpg"
+    },
+    {
+      id: 3,
+      image: "https://api.spicezgold.com/download/file_1734528862995_deel-band-women-rayon-embroidered-kurta-pant-dupatta-set-product-images-rvotwal6de-0-202404071601.webp"
+    },
+    {
+      id: 4,
+      image: "https://api.spicezgold.com/download/file_1734529363003_gosriki-women-s-pink-ethnic-motifs-printed-kurta-with-trouser-dupatta-product-images-rvpkyh5qdr-1-202310141511.jpg"
+    },
+    {
+      id: 5,
+      image: "https://api.spicezgold.com/download/file_1734529363005_gosriki-women-s-pink-ethnic-motifs-printed-kurta-with-trouser-dupatta-product-images-rvpkyh5qdr-2-202310141511.jpg"
+    },
+  ];
+
+  const Zoom = InnerImageZoom.default || InnerImageZoom;
+
   return (
-    <>
-      <Zoom
-        src="https://api.spicezgold.com/download/file_1734528862995_deel-band-women-rayon-embroidered-kurta-pant-dupatta-set-product-images-rvotwal6de-0-202404071601.webp"
-        
-        zoomType="hover"
-        zoomScale={1.5}
-      />
-    </>
+    <div className="flex gap-3">
+
+      {/* Thumbnail Slider */}
+      <div className="w-[20%] slider">
+        <Swiper
+          ref={zoomSliderSml}
+          slidesPerView={4}
+          spaceBetween={1}
+          loop={true}
+          direction="vertical"
+          navigation
+          modules={[Navigation]}
+          className="!h-[76vh] ZoomProductSliderThumbs"
+        >
+          {ProductCatSlider.map((item, index) => (
+            <SwiperSlide key={item.id}>
+              <div
+                onClick={() => goto(index)}
+                className={`rounded-md overflow-hidden cursor-pointer group border transition-all duration-300 
+                  ${slideIndex === index ? "border-red-500 opacity-100" : "border-transparent opacity-30"}
+                `}
+              >
+                <img
+                  src={item.image}
+                  alt=""
+                  className="w-full transition-all duration-300 group-hover:scale-105"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      {/* Big Zoom Slider */}
+      <div className="w-[80%] zoomcontainer h-[500px] overflow-hidden rounded-md">
+        <Swiper
+          ref={zoomSliderBig}
+          slidesPerView={1}
+          spaceBetween={0}
+          className="ZoomProductSliderThumbs h-full"
+        >
+          {ProductCatSlider.map((item) => (
+            <SwiperSlide key={item.id}>
+              <Zoom
+                src={item.image}
+                zoomType="hover"
+                zoomScale={1}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+    </div>
   );
 };
 
